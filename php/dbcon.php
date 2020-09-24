c03340cf74387f21ac98ffb458e85fcd132efe15
HYERYEONG KANG <khr6604@gmail.com>
2020-09-24 14:22:00 +0900

    first commit

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
     $host = 'localhost';
-    $username = 'kang'; # MySQL 계정 아이디
-    $password = 'kang0908'; # MySQL 계정 패스워드
-    $dbname = 'TLO';  # DATABASE 이름
+    $username = 'root'; # MySQL 계정 아이디
+    $password = ''; # MySQL 계정 패스워드
+    $dbname = 'logintest';  # DATABASE 이름
 
 
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
